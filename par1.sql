75dfd833d57c0c8953ffab18d8842c3949110ed1
alzabirr <alzabirhussain@gmail.com>
2024-10-08 03:05:25 +0600

    first commit

---
+++ par1.sql
@@ -0,0 +1,7 @@
+-- how to write INSERT query  
+
+
+INSERT INTO employees
+(`firstName`, `lastName`, `City`, `Phone`, `Email`, `Salary`)
+VALUES
+('Al', 'Zabir', 'Alamdanga', '01715483823', 'alzabir@gmail.com', '40000' )
